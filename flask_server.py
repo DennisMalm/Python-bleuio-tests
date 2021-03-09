414bbbf79a7efad07f5f64956a4a00247f217862
DennisMalm <dennis.malm@gmail.com>
2021-03-09 13:31:34 +0100

    Test

--- flask_server.py
+++ flask_server.py
@@ -1,15 +1,23 @@
-import random
-import serial
 import time
-from flask import Flask, render_template
+import random
+import numpy as np
+from flask import Flask, render_template, redirect, url_for
 from test_list import tests
 
+# Flask
 app = Flask(__name__)
 
-
+test = np.random.rand()
+# Routes
 @app.route('/')
 def base():
-    return render_template('base.html')
+    return render_template('base.html', test=test)
+
+
+@app.route('/start', methods=["POST"])
+def start_tests():
+    global test
+    test = np.random.rand()
 
 
 if __name__ == "__main__":
